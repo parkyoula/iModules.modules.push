8c675436097d88a6bb06e025661ae854056bfab1
arzzcom <arzz@arzz.com>
2024-09-06 14:54:26 +0900

    기본구조 정의

---
+++ Push.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 이 파일은 아이모듈 알림모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 알림모듈 클래스 정의한다.
+ *
+ * @file /modules/push/Push.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2024. 9. 6.
+ */
+namespace modules\push;
+class Push extends \Module
+{
+}
